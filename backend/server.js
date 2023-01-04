8a649ff4015a8d0417839830b6b25b5032631696
Teaver <jerzyx11@gmail.com>
2023-01-04 21:29:39 +0100

    learning middleware

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,40 @@
-const app = require("express")()
+require('dotenv').config()
+const redis = require("redis")
+const express = require("express")
 const axios = require("axios")
 const cors = require("cors")
-const redis = require("redis")
-require('dotenv').config()
+const app = express()
+const PORT = 3000
+
+app.use( express.json() )
 
 // redis://<username>:<password>@<public-endpoint>:<port>
-
 const client = redis.createClient({
     url: `redis://${process.env.USER}:${process.env.PASSWORD}@${process.env.ENDPOINT}:${process.env.PORT}`
 })
 
 const main = async () => {
+    // Start express server
+    app.listen(PORT, () => {
+        console.log(`Express server started on http://localhost:${PORT}`)
+    })
+
+    // Connect to redis
     client.connect()
-    const test = await client.aclWhoAmI()
-    console.log(test)
-    console.log('done')
+    const user = await client.aclWhoAmI()
+    console.log(`Logged to redis as "${user}"`)
     return
 }
+
+app.get('/userprofile', (req, res) => {
+    res.status(200).send({
+        username: 'user123',
+        id: '0001',
+        settings: {
+            dark_mode: true,
+            tile_color_id: 2,
+        },
+    })
+})
 
 main()
