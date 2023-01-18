831130fc73e6baa803b9bb773b84000c8e1eaab8
teaver <jerzyx11@gmail.com>
2023-01-18 15:29:29 +0100

    heatmap sample size 800

--- backend/jupyter/utils/generate_test_data.js
+++ backend/jupyter/utils/generate_test_data.js
@@ -3,8 +3,9 @@ import generate_data from "./generate_data.js";
 // Generate a lot of testing data for jupyter
 const generate_test_data = () => {
     let data = {}
+    const amount_of_games = 800
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < amount_of_games; i++) {
         const generated_game_data = generate_data()
         data[i] = generated_game_data
     }
