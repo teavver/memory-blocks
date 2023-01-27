343087a3d2ab5bddc454981156bf9acc99791043
teaver <jerzyx11@gmail.com>
2023-01-27 19:52:17 +0100

    post heatmap after game, heatmap & leaderboard stable

--- src/utils/api/POST_game.js
+++ src/utils/api/POST_game.js
@@ -4,13 +4,12 @@ import id_generator from "../game/id_generator";
 const POST_game = (game_data) => {
     
     const game_id = id_generator(6)
-
     const game_data_final = {
         ...game_data,
         game_id
     }
 
-    axios.post(`http://localhost:3000/users/${game_data.user_id}/games/${game_id}`, game_data_final )
+    axios.post(`http://localhost:3000/games/${game_id}`, game_data_final )
     .then(function (res) {
         console.log(res)
     })
