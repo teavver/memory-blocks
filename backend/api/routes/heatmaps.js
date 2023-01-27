4f982c8ec072a3f50c82e4b1af820b6c0a6bc96d
teaver <jerzyx11@gmail.com>
2023-01-27 16:16:32 +0100

    heatmap almost fixed (weird lines appear wtf?)

--- backend/api/routes/heatmaps.js
+++ backend/api/routes/heatmaps.js
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { spawn } from "child_process";
 import redis_get_json from "../../redis/calls/redis_get_json.js";
 import redis_update_heatmap from "../../redis/calls/redis_update_heatmap.js";
+import redis_create_heatmap from "../../redis/calls/redis_create_heatmap.js";
 
 const heatmaps = Router()
 
 const GET_heatmaps = async (req, res) => {
     const user_id = req.params.userId
+    redis_create_heatmap(user_id)
     const heatmap_data = await redis_get_json(`heatmap:${user_id}`)
+    console.log(heatmap_data)
 
     var base64_data
     const python = spawn('python', ['./heatmaps/generate_heatmap.py', heatmap_data, user_id])
@@ -23,7 +26,7 @@ const GET_heatmaps = async (req, res) => {
     }) 
 }
 
-const POST_heatmaps = async (req,res) => {
+const POST_heatmaps = async (req, res) => {
     const user_id = req.params.userId
     const index_obj = req.body
     await redis_update_heatmap(user_id, index_obj)
